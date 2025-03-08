f1433c43663831b7df54308f0a21a3464410d64d
roge111 <64649951+roge111@users.noreply.github.com>
2025-03-08 10:37:37 +0300

    Избавляемся от изменяемых переменных

--- Interpolation/Newton.fs
+++ Interpolation/Newton.fs
@@ -1,17 +1,27 @@
 module Newton
 
-// Функция интерполяции методом Ньютона
+// Функция интерполяции методом Ньютона без использования mutable
 let newtonInterpolation (points: (float * float) array) (x: float) : float =
     let n = points.Length
-    let dividedDifferences = Array.init n (fun i -> points.[i].Item2)
     
-    for j in 1 .. n - 1 do
-        for i in n - 1 .. -1 .. j do
-            dividedDifferences.[i] <- (dividedDifferences.[i] - dividedDifferences.[i - 1]) / (points.[i].Item1 - points.[i - j].Item1)
+    // Вычисление разделённых разностей
+    let dividedDifferences =
+        let rec calculateDifferences (differences: float array) j =
+            if j = n then differences
+            else
+                let updatedDifferences = 
+                    Array.init n (fun i -> 
+                        if i < j then differences.[i]
+                        else 
+                            (differences.[i] - differences.[i - 1]) / (points.[i].Item1 - points.[i - j].Item1))
+                calculateDifferences updatedDifferences (j + 1)
+        calculateDifferences (Array.map snd points) 1
     
-    let mutable result = dividedDifferences.[0]
-    let mutable product = 1.0
-    for i in 1 .. n - 1 do
-        product <- product * (x - points.[i - 1].Item1)
-        result <- result + dividedDifferences.[i] * product
-    result
+    // Вычисление значения интерполяции
+    let rec computeResult i product result =
+        if i = n then result
+        else
+            let newProduct = product * (x - points.[i - 1].Item1)
+            computeResult (i + 1) newProduct (result + dividedDifferences.[i] * newProduct)
+    
+    computeResult 1 1.0 dividedDifferences.[0]
